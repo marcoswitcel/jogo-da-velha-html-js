f8a5db278de93d39b03ff971a5d32a262e8c033b
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-09-22 22:15:29 -0300

    planejando estrutura

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 
 console.log("Olá mundo do 'Jogo da Velha' em HTML");
+
+const btnPlay = document.getElementById('btn-action-play');
+btnPlay.addEventListener('click', () => {
+  const element = document.querySelector('.app-screen.in-game');
+  element.classList.add('is-visible');
+  element.classList.remove('is-hidden');
+});
