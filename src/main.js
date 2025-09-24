050d77e98b2a47f7d5dab6772973654fb2a84440
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-09-23 22:10:18 -0300

    WIP: design da solução de telas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 
 console.log("Olá mundo do 'Jogo da Velha' em HTML");
 
-const btnPlay = document.getElementById('btn-action-play');
-btnPlay.addEventListener('click', () => {
-  const element = document.querySelector('.app-screen.in-game');
-  element.classList.add('is-visible');
-  element.classList.remove('is-hidden');
+const getById = (id) => document.getElementById(id);
+
+class View {
+  /**
+   * @type {HTMLTemplateElement}
+   */
+  template;
+  /**
+   * @type {HTMLElement}
+   */
+  rootElement;
+
+  constructor(name, template) {
+    this.viewName = name;
+    this.template = template;
+    this.rootElement = template.conten.cloneNode(true);
+  }
+
+  setup() {}
+}
+
+const appRoot = getById('app');
+const views = [ getById('app-view-menu'), getById('app-view-in-game'), ];
+
+const changeToView = viewName => {
+  appRoot.innerHTML = ''; // @todo Otimizar isso aqui
+  appRoot.appendChild(getById(viewName).content.cloneNode(true));
+}
+
+changeToView('app-view-menu');
+
+getById('btn-action-play').addEventListener('click', () => {
+  changeToView('app-view-in-game');
 });
