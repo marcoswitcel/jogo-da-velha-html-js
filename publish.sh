5d7c04e87270b39690194fe7f5eeb52ff4c9a152
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-10-13 21:30:02 -0300

    scripts ...

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+LINE="========================================="
+
+echo "git checkout gh-pages"
+git checkout gh-pages
+echo $LINE
+
+echo "git pull origin main"
+git pull origin main
+echo $LINE
+
+echo "git push"
+git push
+echo $LINE
+
+echo "git checkout main"
+git checkout main
+echo $LINE
