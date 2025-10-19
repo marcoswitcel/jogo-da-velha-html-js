5bd8ee61bf1c0b4876d460dc3227b024609897a5
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-10-19 15:03:32 -0300

    WIP: seletor do "tema"

--- src/view/config-view.js
+++ src/view/config-view.js
@@ -4,5 +4,31 @@ import { View } from './view.js';
 export class ConfigView extends View {
   constructor(ctx) {
     super('app-view-config', getById('app-view-config'), ctx, 'Configurações');
+
+  }
+
+  setup() {
+    this.query('#theme-mode')[0].addEventListener('change', e => {
+      /**
+       * @type {HTMLSelectElement}
+       */ //@ts-ignore
+      const target = e.target; 
+      const value = target.value;
+      
+      if (value === 'Dark') {
+        // força "modo escuro" com com a classe 'dark-mode-on'
+        this.ctx.rootElement.classList.add('dark-mode-on')
+        this.ctx.rootElement.classList.remove('system-preferences-theme');
+      } else if (value === 'Inherit') {
+        // força "herdar do sistema" com com a classe 'system-preferences-theme', esta classe depende de estar
+        // ativado o "modo escuro" para aplicar o tema do "modo escuro"
+        this.ctx.rootElement.classList.add('system-preferences-theme');
+        this.ctx.rootElement.classList.remove('dark-mode-on')
+      } else {
+        // O padrão é o "modo claro"
+        this.ctx.rootElement.classList.remove('dark-mode-on');
+        this.ctx.rootElement.classList.remove('system-preferences-theme');
+      }
+    })
   }
 }
